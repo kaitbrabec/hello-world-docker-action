92be6119a3fea24fbb9ea0985241c05e24e0fa1a
kaitbrabec <99047761+kaitbrabec@users.noreply.github.com>
2023-02-19 19:40:40 +0000

    Update app.py

--- source/app.py
+++ source/app.py
@@ -2,5 +2,5 @@
 print("Hello World")
 import sys
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+print (f"Number of arguments: {len(sys.argv)}")
+print (f'Argument List:', {str(sys.argv)}")
