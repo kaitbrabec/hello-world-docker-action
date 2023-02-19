c361169aba0761db1c1e0f21e554232992ec5e31
kaitbrabec <99047761+kaitbrabec@users.noreply.github.com>
2023-02-19 19:42:25 +0000

    Update app.py

--- source/app.py
+++ source/app.py
@@ -1,6 +1,6 @@
-
-print("Hello World")
 import sys
+print("Hello World")
 
-print (f"Number of arguments: {len(sys.argv)}")
-print (f'Argument List:', {str(sys.argv)}")
+
+print(f"Number of arguments: {len(sys.argv)}")
+print(f"Argument List: {str(sys.argv)}")
